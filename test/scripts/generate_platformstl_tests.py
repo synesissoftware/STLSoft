4bb80743ca2d190edf0de4b879e49029bd5b8f56
Matt Wilson <matthew@synesis.com.au>
2026-08-09 09:21:56 +1000

    fix

--- test/scripts/generate_platformstl_tests.py
+++ test/scripts/generate_platformstl_tests.py
@@ -1205,12 +1205,14 @@ static void test_assign_and_c_str(void)
     write(
         TEST_COMPONENT / "filesystem/test.component.platformstl.filesystem.filesystem_traits/entry.cpp",
         """#include <platformstl/filesystem/filesystem_traits.hpp>
+#include <platformstl/filesystem/path.hpp>
+#include <xtests/xtests.h>
 #include <xtests/terse-api.h>
 #include <xtests/util/temp_directory.hpp>
 #include <stlsoft/stlsoft.h>
 #include <stdlib.h>
 
-namespace { static void test_file_exists_current_dir(void); }
+namespace { static void test_file_exists(void); }
 
 int main(int argc, char* argv[])
 {
@@ -1219,20 +1221,31 @@ int main(int argc, char* argv[])
     XTESTS_COMMANDLINE_PARSEVERBOSITY(argc, argv, &verbosity);
     if (XTESTS_START_RUNNER("test.component.platformstl.filesystem.filesystem_traits", verbosity))
     {
-        XTESTS_RUN_CASE(test_file_exists_current_dir);
+        XTESTS_RUN_CASE(test_file_exists);
         XTESTS_PRINT_RESULTS();
         XTESTS_END_RUNNER_UPDATE_EXITCODE(&retCode);
     }
     return retCode;
 }
 
-static void test_file_exists_current_dir(void)
+namespace {
+
+static void test_file_exists(void)
 {
     typedef platformstl::filesystem_traits<char> fs_traits_t;
     using ::xtests::cpp::util::temp_directory;
+
     temp_directory td(temp_directory::EmptyOnOpen | temp_directory::EmptyOnClose | temp_directory::RemoveOnClose);
-    TEST_BOOLEAN_FALSE(fs_traits_t::file_exists(td.c_str()));
+
+    TEST_BOOLEAN_TRUE(fs_traits_t::file_exists(td.c_str()));
+    TEST_BOOLEAN_TRUE(fs_traits_t::is_directory(td.c_str()));
+
+    platformstl::path_a missing(td.c_str());
+    missing /= "does-not-exist";
+
+    TEST_BOOLEAN_FALSE(fs_traits_t::file_exists(missing.c_str()));
 }
+} // anonymous namespace
 """,
     )
 
