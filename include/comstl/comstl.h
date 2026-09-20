d96191d8af4fcea7bd2b2071fe6612999d93ef6d
synesissoftware <matthew@synesis.com.au>
2026-09-20 22:52:33 +1000

    Boilerplate, CI (#15)
    * chore(c-cpp): union files.associations from ~/dev vscode settings
    
    Give the 28 in-wave C/C++ projects (and gold **c** / **cxx** / **c_cxx**
    templates) a sorted **files.associations** union of all associations
    found under **~/dev**;
    
    * .gitattributes
    
    * .vscode/settings.json
    
    * .vimrc
    
    * CHANGES.md
    
    * chore(<lib>): use computed PREFIX_VER with documented PATCH
    
    Align library version macros to c-cpp-standards. Define MAJOR, MINOR,
    PATCH, and ALPHABETA; compute PREFIX_VER by shifts; keep REVISION as a
    skip-section alias of PATCH (target at column 60); retain named historical
    VER_* constants only for comparison and documentation.
    
    * squash-commit
    
    * squash-commit

--- include/comstl/comstl.h
+++ include/comstl/comstl.h
@@ -5,11 +5,11 @@
  *          platform discriminations, and definitions of types.
  *
  * Created: 15th January 2002
- * Updated: 13th October 2024
+ * Updated: 17th September 2026
  *
  * Home:    http://stlsoft.org/
  *
- * Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
+ * Copyright (c) 2019-2026, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2002-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -55,8 +55,8 @@
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define COMSTL_VER_COMSTL_H_COMSTL_MAJOR       3
 # define COMSTL_VER_COMSTL_H_COMSTL_MINOR       9
-# define COMSTL_VER_COMSTL_H_COMSTL_REVISION    9
-# define COMSTL_VER_COMSTL_H_COMSTL_EDIT        133
+# define COMSTL_VER_COMSTL_H_COMSTL_REVISION    10
+# define COMSTL_VER_COMSTL_H_COMSTL_EDIT        134
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 /** \file comstl/comstl.h
@@ -68,32 +68,37 @@
 /* /////////////////////////////////////////////////////////////////////////
  * COMSTL version
  *
- * The libraries version information is comprised of major, minor and revision
+ * The libraries version information is comprised of major, minor and patch
  * components.
  *
- * The major version is denoted by the _COMSTL_VER_MAJOR preprocessor symbol.
- * A change to the major version component implies that a dramatic change has
- * occurred in the libraries, such that considerable changes to source dependent
- * on previous versions would need to be effected.
+ * The major version is denoted by the _COMSTL_VER_MAJOR preprocessor
+ * symbol. A change to the major version component implies that a dramatic
+ * change has occurred in the libraries, such that considerable changes to
+ * source dependent on previous versions would need to be effected.
  *
- * The minor version is denoted by the _COMSTL_VER_MINOR preprocessor symbol.
- * Changes to the minor version component imply that a significant change has
- * occurred to the libraries, either in the addition of new functionality or in
- * the destructive change to one or more components such that recomplilation and
- * code change may be necessitated.
+ * The minor version is denoted by the _COMSTL_VER_MINOR preprocessor
+ * symbol. Changes to the minor version component imply that a significant
+ * change has occurred to the libraries, either in the addition of new
+ * functionality or in the destructive change to one or more components such
+ * that recompilation and code change may be necessitated.
  *
- * The revision version is denoted by the _COMSTL_VER_REVISION preprocessor
- * symbol. Changes to the revision version component imply that a bug has been
- * fixed. Dependent code should be recompiled in order to pick up the changes.
+ * The patch version is denoted by the _COMSTL_VER_PATCH preprocessor
+ * symbol. Changes to the patch version component imply that a bug has been
+ * fixed. Dependent code should be recompiled in order to pick up the
+ * changes.
  *
  * In addition to the individual version symbols - _COMSTL_VER_MAJOR,
- * _COMSTL_VER_MINOR and _COMSTL_VER_REVISION - a composite symbol _COMSTL_VER
- * is defined, where the upper 8 bits are 0, bits 16-23 represent the major
- * component,  bits 8-15 represent the minor component, and bits 0-7 represent
- * the revision component.
+ * _COMSTL_VER_MINOR and _COMSTL_VER_PATCH - a composite symbol _COMSTL_VER
+ * is defined, where:
+ *  - bits 24-31: the major version
+ *  - bits 16-23: the minor version
+ *  - bits 8-15: the patch version
+ *  - bits 0-7: the alphabeta / prerelease number; if not a prerelease,
+ *    it is 0xFF
  *
- * Each release of the libraries will bear a different version, and that version
- * will also have its own symbol: Version 1.0.1 specifies _COMSTL_VER_1_0_1.
+ * Each release of the libraries will bear a different version, and that
+ * version will also have its own symbol: Version 1.0.1 specifies
+ * _COMSTL_VER_1_0_1.
  *
  * Thus the symbol _COMSTL_VER may be compared meaningfully with a specific
  * version symbol, e.g. #if _COMSTL_VER >= _COMSTL_VER_1_0_1
@@ -115,9 +120,15 @@
  * substantial as to require wholesale changes to application code.
  */
 
-/** \def _COMSTL_VER_REVISION
+/** \def _COMSTL_VER_PATCH
  *
- * The revision version number of COMSTL
+ * The patch version number of COMSTL
+ */
+
+/** \def _COMSTL_VER_ALPHABETA
+ *
+ * The alpha/beta number of COMSTL, in the range 1-0xFE for prerelease
+ * versions, and 0xFF for a released version
  */
 
 /** \def _COMSTL_VER
@@ -163,8 +174,20 @@
 
 #define _COMSTL_VER_MAJOR       1
 #define _COMSTL_VER_MINOR       10
-#define _COMSTL_VER_REVISION    3
-#define _COMSTL_VER             _COMSTL_VER_1_10_3
+#define _COMSTL_VER_PATCH       3
+#define _COMSTL_VER_ALPHABETA   0xFF
+
+#define _COMSTL_VER \
+    (0\
+        |   (   _COMSTL_VER_MAJOR       << 24   ) \
+        |   (   _COMSTL_VER_MINOR       << 16   ) \
+        |   (   _COMSTL_VER_PATCH       <<  8   ) \
+        |   (   _COMSTL_VER_ALPHABETA   <<  0   ) \
+    )
+
+#ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
+# define _COMSTL_VER_REVISION                               _COMSTL_VER_PATCH
+#endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 
 /* /////////////////////////////////////////////////////////////////////////
