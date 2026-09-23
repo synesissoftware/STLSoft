90b2e1e62786d57bc40d8b64e67c44392a84086d
Matt Wilson <matthew@synesis.com.au>
2026-09-23 17:30:30 +1000

    feat(platformstl): add FILE_stream::write_binary()
    Add a byte-oriented write that preserves embedded NULs, and deprecate
    write(void const*, size_t) in its favour. Cover CR and CRLF files in the
    file_lines component tests, and write the NUL fixture through
    write_binary().

--- test/component/platformstl/filesystem/test.component.platformstl.filesystem.file_lines/entry.cpp
+++ test/component/platformstl/filesystem/test.component.platformstl.filesystem.file_lines/entry.cpp
@@ -32,10 +32,6 @@
 #include <platformstl/exception/invalid_file_type_exception.hpp>
 #include <platformstl/filesystem/FILE_stream.hpp>
 
-/* Standard C++ header files */
-#include <fstream>
-#include <string>
-
 /* Standard C header files */
 #include <stdlib.h>
 
@@ -50,6 +46,12 @@ namespace {
     void TEST_FILE_WITH_ONE_LINE_UNTERMINATED();
     void TEST_FILE_WITH_ONE_LINE_TERMINATED();
     void TEST_FILE_WITH_THREE_LINES();
+    void TEST_FILE_WITH_ONE_LINE_TERMINATED_BY_CRLF();
+    void TEST_FILE_WITH_THREE_LINES_SEPARATED_BY_CRLF();
+    void TEST_FILE_WITH_CONSECUTIVE_CRLF_GIVES_EMPTY_LINE();
+    void TEST_FILE_WITH_ONE_LINE_TERMINATED_BY_CR();
+    void TEST_FILE_WITH_THREE_LINES_SEPARATED_BY_CR();
+    void TEST_FILE_WITH_CONSECUTIVE_CR_GIVES_EMPTY_LINE();
     void TEST_FILE_CONTAINING_NUL_CHARACTER_THROWS();
 } // anonymous namespace
 
@@ -71,6 +73,12 @@ int main(int argc, char* argv[])
         XTESTS_RUN_CASE(TEST_FILE_WITH_ONE_LINE_UNTERMINATED);
         XTESTS_RUN_CASE(TEST_FILE_WITH_ONE_LINE_TERMINATED);
         XTESTS_RUN_CASE(TEST_FILE_WITH_THREE_LINES);
+        XTESTS_RUN_CASE(TEST_FILE_WITH_ONE_LINE_TERMINATED_BY_CRLF);
+        XTESTS_RUN_CASE(TEST_FILE_WITH_THREE_LINES_SEPARATED_BY_CRLF);
+        XTESTS_RUN_CASE(TEST_FILE_WITH_CONSECUTIVE_CRLF_GIVES_EMPTY_LINE);
+        XTESTS_RUN_CASE(TEST_FILE_WITH_ONE_LINE_TERMINATED_BY_CR);
+        XTESTS_RUN_CASE(TEST_FILE_WITH_THREE_LINES_SEPARATED_BY_CR);
+        XTESTS_RUN_CASE(TEST_FILE_WITH_CONSECUTIVE_CR_GIVES_EMPTY_LINE);
         XTESTS_RUN_CASE_THAT_THROWS(TEST_FILE_CONTAINING_NUL_CHARACTER_THROWS, platformstl::invalid_file_type_exception);
 
         XTESTS_PRINT_RESULTS();
@@ -151,15 +159,119 @@ void TEST_FILE_WITH_THREE_LINES()
     TEST_MS_EQ("ghi", lines[2]);
 }
 
+void TEST_FILE_WITH_ONE_LINE_TERMINATED_BY_CRLF()
+{
+    temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
+
+    {
+        platformstl::FILE_stream stm(f.c_str(), "wb");
+
+        stm.write("abc\r\n");
+    }
+
+    platformstl::file_lines lines(f.c_str());
+
+    REQUIRE(TEST_INT_EQ(1u, lines.size()));
+    TEST_MS_EQ("abc", lines[0]);
+}
+
+void TEST_FILE_WITH_THREE_LINES_SEPARATED_BY_CRLF()
+{
+    temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
+
+    {
+        platformstl::FILE_stream stm(f.c_str(), "wb");
+
+        stm.write("abc\r\ndef\r\nghi\r\n");
+    }
+
+    platformstl::file_lines lines(f.c_str());
+
+    REQUIRE(TEST_INT_EQ(3u, lines.size()));
+    TEST_MS_EQ("abc", lines[0]);
+    TEST_MS_EQ("def", lines[1]);
+    TEST_MS_EQ("ghi", lines[2]);
+}
+
+void TEST_FILE_WITH_CONSECUTIVE_CRLF_GIVES_EMPTY_LINE()
+{
+    temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
+
+    {
+        platformstl::FILE_stream stm(f.c_str(), "wb");
+
+        stm.write("abc\r\n\r\nghi");
+    }
+
+    platformstl::file_lines lines(f.c_str());
+
+    REQUIRE(TEST_INT_EQ(3u, lines.size()));
+    TEST_MS_EQ("abc", lines[0]);
+    TEST_MS_EQ("", lines[1]);
+    TEST_MS_EQ("ghi", lines[2]);
+}
+
+void TEST_FILE_WITH_ONE_LINE_TERMINATED_BY_CR()
+{
+    temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
+
+    {
+        platformstl::FILE_stream stm(f.c_str(), "wb");
+
+        stm.write("abc\r");
+    }
+
+    platformstl::file_lines lines(f.c_str());
+
+    REQUIRE(TEST_INT_EQ(1u, lines.size()));
+    TEST_MS_EQ("abc", lines[0]);
+}
+
+void TEST_FILE_WITH_THREE_LINES_SEPARATED_BY_CR()
+{
+    temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
+
+    {
+        platformstl::FILE_stream stm(f.c_str(), "wb");
+
+        stm.write("abc\rdef\rghi\r");
+    }
+
+    platformstl::file_lines lines(f.c_str());
+
+    REQUIRE(TEST_INT_EQ(3u, lines.size()));
+    TEST_MS_EQ("abc", lines[0]);
+    TEST_MS_EQ("def", lines[1]);
+    TEST_MS_EQ("ghi", lines[2]);
+}
+
+void TEST_FILE_WITH_CONSECUTIVE_CR_GIVES_EMPTY_LINE()
+{
+    temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
+
+    {
+        platformstl::FILE_stream stm(f.c_str(), "wb");
+
+        stm.write("abc\r\rghi");
+    }
+
+    platformstl::file_lines lines(f.c_str());
+
+    REQUIRE(TEST_INT_EQ(3u, lines.size()));
+    TEST_MS_EQ("abc", lines[0]);
+    TEST_MS_EQ("", lines[1]);
+    TEST_MS_EQ("ghi", lines[2]);
+}
+
 void TEST_FILE_CONTAINING_NUL_CHARACTER_THROWS()
 {
     temp_file f(temp_file::DeleteOnClose | temp_file::EmptyOnOpen | temp_file::CloseOnOpen);
 
     {
-        std::ofstream       stm(f.c_str(), std::ios::binary | std::ios::trunc);
-        std::string const   contents("abc\0def", 7);
+        char const                  contents[]  =   { 'a', 'b', 'c', '\0', 'd', 'e', 'f' };
+        platformstl::FILE_stream    stm(f.c_str(), "wb");
 
-        stm.write(contents.data(), static_cast<std::streamsize>(contents.size()));
+        stm.write_binary(contents, sizeof(contents));
     }
 
     platformstl::file_lines lines(f.c_str());
